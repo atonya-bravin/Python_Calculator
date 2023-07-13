835ab23230087e326df0df6bc4cf0f3774d044e5
atonya-bravin <bravinatonya.brani@gmail.com>
2023-07-13 05:47:47 +0300

    python_calcualtor styled using pycodestyle

--- python_calculator_helpers/python_calculator.py
+++ python_calculator_helpers/python_calculator.py
@@ -1,36 +1,43 @@
 """
-This module contains a class that holds the helper methods to help with calculations
+This module contains a class that holds the helper
+methods to help with calculations
 """
+
+
 class PythonCalculator():
     """
     The class that holds the methods that help with the calculations
     """
 
-    def sum(first_number: int, second_number: int) -> int:
+    def sum(self, first_number: int, second_number: int) -> int:
         """
-        This is a function that takes in two numbers and returns to us their sum
+        This is a function that takes in two numbers
+        and returns to us their sum
         """
         return first_number + second_number
-    
-    def dif(first_number: int, second_number: int) -> int:
+
+    def dif(self, first_number: int, second_number: int) -> int:
         """
-        This is a function that takes in two numbers and returns to us their difference
+        This is a function that takes in two
+        numbers and returns to us their difference
         """
         return first_number - second_number
-    
-    def mult(first_number: int, second_number: int) -> int:
+
+    def mult(self, first_number: int, second_number: int) -> int:
         """
-        This is a function that takes in two numbers and returns to us their product
+        This is a function that takes in two
+        numbers and returns to us their product
         """
         return first_number * second_number
-    
-    def div(first_number: int, second_number: int) -> float:
+
+    def div(self, first_number: int, second_number: int) -> float:
         """
-        This is a function that takes in two numbers and returns to us their quatient
+        This is a function that takes in two
+        numbers and returns to us their quatient
         """
         return first_number / second_number
-    
-    def rem(first_number: int, second_number: int) -> int:
+
+    def rem(self, first_number: int, second_number: int) -> int:
         """
         This is a function that takes in two numbers and returns to us the
         reminder after their division
