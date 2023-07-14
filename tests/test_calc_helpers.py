97805b24dc2e59adfdfc579c194105ecc25c757f
atonya-bravin <bravinatonya.brani@gmail.com>
2023-07-14 04:00:37 +0300

    passing tests for the methods: dif, mult, and div

--- tests/test_calc_helpers.py
+++ tests/test_calc_helpers.py
@@ -13,12 +13,25 @@ class CalculatorHelpersTester(TestCase):
     helper methods of our calculator
     """
 
-    py_calc = PythonCalculator(10, 20)
+    def setUp(self):
+        self.py_calc = PythonCalculator(10, 20)
 
     def test_sum(self):
         self.assertEqual(self.py_calc.sum(), 30,
                          "There is an issue calculating the sum!!")
-
-
+    
+    def test_dif(self):
+        self.assertEqual(self.py_calc.dif(), -10,
+                         "There is an issue calculating the difference!!")
+        
+    def test_mult(self):
+        self.assertEqual(self.py_calc.mult(), 200,
+                         "There is an issue calculating the product!!")
+        
+    def test_div(self):
+        self.assertEqual(self.py_calc.div(), 0.5,
+                         "There is an issue calfulating the quatient!!")
+        
+    
 if __name__ == '__main__':
     unittest.main()
